366b059ed3f01e34a3d71ff0d06d2fe7ef627a63
elong <elongliks@gmail.com>
2017-11-24 20:16:29 +0200

    progress

--- src/Header/Exchange.js
+++ src/Header/Exchange.js
@@ -1,50 +1,50 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { Container, Button, Input, Label, Icon } from 'semantic-ui-react'
+import React, { Component } from "react";
+import axios from "axios";
+import { Container, Button, Input, Label, Icon } from "semantic-ui-react";
 export default class extends Component {
   state = {
-    youGive: '',
-    youGet: '',
-    ILStoBTC: ''
-  }
+    youGive: "",
+    youGet: "",
+    ILStoBTC: ""
+  };
 
   componentDidMount = () => {
-    this.getCurrentRate()
-    this.youGive.focus()
-  }
+    this.getCurrentRate();
+    this.youGive.focus();
+  };
   getCurrentRate = async () => {
     let { data: { quotes } } = await axios.get(
-      'http://www.apilayer.net/api/live?access_key=acafc0bb45eef112ab535b81dfc1116b'
-    )
-    this.setState({ ILStoBTC: 1 / quotes.USDILS * quotes.USDBTC })
-  }
+      "http://www.apilayer.net/api/live?access_key=acafc0bb45eef112ab535b81dfc1116b"
+    );
+    this.setState({ ILStoBTC: 1 / quotes.USDILS * quotes.USDBTC });
+  };
   handleExchangeFromILStoBTC = event => {
-    const { ILStoBTC } = this.state
-    const give = event.target.value
-    const get = give * ILStoBTC
-    this.setState({ youGive: give, youGet: get })
-  }
+    const { ILStoBTC } = this.state;
+    const give = event.target.value;
+    const get = give * ILStoBTC;
+    this.setState({ youGive: give, youGet: get });
+  };
   handleExchangeFromBTCtoILS = event => {
-    const BTCtoILS = Math.pow(this.state.ILStoBTC, -1)
-    const get = event.target.value
-    const give = get * BTCtoILS
-    this.setState({ youGet: get, youGive: give })
-  }
+    const BTCtoILS = Math.pow(this.state.ILStoBTC, -1);
+    const get = event.target.value;
+    const give = get * BTCtoILS;
+    this.setState({ youGet: get, youGive: give });
+  };
   checkIfNumber = event => {
-    if (!(event.charCode >= 48 && event.charCode <= 57)) event.preventDefault()
-  }
+    if (!(event.charCode >= 48 && event.charCode <= 57)) event.preventDefault();
+  };
 
   render() {
     return (
-      <Container style={{ paddingTop: '2em' }}>
+      <Container style={{ paddingTop: "2em" }}>
         <Input
           labelPosition="right"
           placeholder="You Give"
           size="huge"
           style={{
-            marginRight: '0.5em',
+            marginRight: "0.5em",
             opacity: 0.9,
-            color: '#FFFF00'
+            color: "#FFFF00"
           }}
           value={this.state.youGive}
           ref={ref => (this.youGive = ref)}
@@ -59,14 +59,14 @@ export default class extends Component {
         <Icon
           name="exchange"
           size="big"
-          style={{ color: 'lightGreen', marginBottom: 10 }}
+          style={{ color: "lightGreen", marginBottom: 10 }}
         />
 
         <Input
           labelPosition="right"
           placeholder="You Get"
           size="huge"
-          style={{ marginLeft: '0.5em', marginRight: '1em', opacity: 0.9 }}
+          style={{ marginLeft: "0.5em", marginRight: "1em", opacity: 0.9 }}
           value={this.state.youGet}
           onChange={this.handleExchangeFromBTCtoILS}
           onKeyPress={this.checkIfNumber}
@@ -81,14 +81,15 @@ export default class extends Component {
           icon="right arrow"
           labelPosition="right"
           size="huge"
-          color="green"
           style={{
-            paddingTop: '0.9em',
-            paddingBottom: '0.9em'
+            paddingTop: "0.9em",
+            paddingBottom: "0.9em",
+            backgroundColor: "#10d078",
+            color: "white"
           }}
           onClick={() => window.alert(this.state.ILStoBTC)}
         />
       </Container>
-    )
+    );
   }
 }
