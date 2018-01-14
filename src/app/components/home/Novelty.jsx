b4ae74a450f6f3c2697aa5862174be88733f23df
khanh2003 <huynhtrankhanh2003@gmail.com>
2018-01-14 15:30:01 +0700

    Create Section component.

--- src/app/components/home/Novelty.jsx
+++ src/app/components/home/Novelty.jsx
@@ -1,65 +1,28 @@
 import React from 'react'
-import { Segment, Grid, Header, Image, Divider } from 'semantic-ui-react'
+import { SectionHeader, SectionParagraph, SectionContent, CenteredSection } from './Section'
+import BackToTopDivider from './BackToTopDivider'
 
 export default () => (
-  <Segment style={{ padding: '2rem' }} vertical>
-    <Grid centered stackable verticalAlign="middle">
-      <Grid.Row>
-        <Grid.Column width={5}>
-          <Header as="h3" style={{ fontSize: '2rem' }}>
-            Why Flybucks?
-          </Header>
-          <p style={{ fontSize: '1.33rem' }}>
-            We provide individuals and businesses a world class experience to
-            exchange cryptos and digital tokens. Flybucks is the go-to spot for
-            traders who demand lightning fast exchange execution, and best
-            security practices. Whether you are new to cryptocurrencies, or a
-            veteran, Flybucks was created for you!
-          </p>
-        </Grid.Column>
-        <Grid.Column width={4}>
-          <Image src="/assets/images/notifications.svg" />
-        </Grid.Column>
-      </Grid.Row>
-
-      <Grid.Row style={{ padding: 0 }}>
-        <Divider
-          as="h4"
-          horizontal
-          section
-          style={{
-            width: '60%',
-            textTransform: 'uppercase',
-            height: 0
-          }}
-        >
-          <a
-            style={{ color: '#faa61a', cursor: 'pointer' }}
-            onClick={() =>
-              window.scroll({ top: 0, left: 0, behavior: 'smooth' })
-            }
-          >
-            where do I start
-          </a>
-        </Divider>
-      </Grid.Row>
-
-      <Grid.Row>
-        <Grid.Column width={4}>
-          <Image src="/assets/images/cryptocurrencies.svg" />
-        </Grid.Column>
-        <Grid.Column width={5}>
-          <Header as="h3" style={{ fontSize: '2rem', paddingTop: '1.5rem' }}>
-            Today's Money. Cryptocurrencies.
-          </Header>
-          <p style={{ fontSize: '1.33rem' }}>
-            Cryptocurrencies are digital gold. Money that promises to preserve
-            and increase its value over time. Cryptos are also a fast and
-            comfortable means of payment with a worldwide scope, private and
-            anonymous. It's time to ditch paper money. Cryptocurrencies.
-          </p>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </Segment>
+  <CenteredSection>
+    <SectionContent centered image="/assets/images/notifications.svg">
+      <SectionHeader>Why Flybucks?</SectionHeader>
+      <SectionParagraph>
+        We provide individuals and businesses a world class experience to
+        exchange cryptos and digital tokens. Flybucks is the go-to spot for
+        traders who demand lightning fast exchange execution, and best
+        security practices. Whether you are new to cryptocurrencies, or a
+        veteran, Flybucks was created for you!
+      </SectionParagraph>
+    </SectionContent>
+    <BackToTopDivider />
+    <SectionContent centered image="/assets/images/cryptocurrencies.svg">
+      <SectionHeader>Today's Money. Cryptocurrencies.</SectionHeader>
+      <SectionParagraph>
+        Cryptocurrencies are digital gold. Money that promises to preserve
+        and increase its value over time. Cryptos are also a fast and
+        comfortable means of payment with a worldwide scope, private and
+        anonymous. It{"'"}s time to ditch paper money. Cryptocurrencies.
+      </SectionParagraph>
+    </SectionContent>
+  </CenteredSection>
 )
